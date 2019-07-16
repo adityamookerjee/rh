e827b193370275762a4502d8694e29f42513199e
adityamookerjee <aditya.mookerjee@gmail.com>
2019-07-16 00:18:20 -0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,228 @@
+import base64
+import datetime
+import io
+
+import random
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+import sd_material_ui
+import pandas as pd
+import brawl
+
+external_stylesheets = [
+    "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"
+]
+
+
+def return_func_content():
+    return html.Div(
+        [
+            # sd_material_ui.Stepper(
+            #     id='input-stepper',
+            #     activeStep=0,
+            #     stepCount=2,
+            #     stepLabels=['Upload Data', 'Select Fighters'],
+            # ),
+            html.Div(
+                id="upload_div",
+                children=[
+                    dcc.Upload(
+                        id='upload-data',
+                        children=html.Div(['Drag and Drop or ', html.A('Select Files')]),
+                        style={
+                            'width': '100%',
+                            'height': '60px',
+                            'lineHeight': '60px',
+                            'borderWidth': '1px',
+                            'borderStyle': 'dashed',
+                            'borderRadius': '5px',
+                            'textAlign': 'center',
+                            'margin': '10px',
+                        },
+                        # Allow multiple files to be uploaded
+                        multiple=True,
+                    )
+                ],
+            ),
+            # html.Div(
+            #     id="selection_div",
+            #     children=[dcc.Dropdown(id="dropdown_1"), dcc.Dropdown(id="dropdown_2")],
+            # ),
+            html.Div(id='output-data-upload'),
+        ]
+    )
+
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+app.layout = html.Div(
+    [
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.Nav(
+                            children=[
+                                html.Div(
+                                    [
+                                        html.A(
+                                            ["Adi - RH Coding Challenge"],
+                                            className="brand-logo center",
+                                        )
+                                    ],
+                                    className="nav-wrapper",
+                                )
+                            ],
+                            className="light-blue accent-2",
+                        )
+                    ],
+                    className="col s12",
+                )
+            ],
+            className="row",
+        ),
+        return_func_content(),
+    ]
+)
+
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    results = pd.DataFrame()
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+            df.set_index('Name', inplace=True)
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div(['There was an error processing this file.'])
+    applicants = brawl.pick_applicants(df)
+    applicant_pairing = list()
+    for applicant in applicants:
+        applicant_pairing.append(brawl.Applicant(df, applicant))
+    applicant_1 = applicant_pairing[0]
+    applicant_2 = applicant_pairing[1]
+
+    round_num = 1
+    while applicant_1.health > 0 and applicant_2.health > 0:
+        log = brawl.fight(round_num, [applicant_1, applicant_2])
+        round_num += 1
+        results = results.append(log)
+    winner = brawl.determine_winner([applicant_1, applicant_2])
+    return html.Div(
+        [
+            html.Div(
+                [
+                    html.Div(
+                        [
+                            html.Div(
+                                [
+                                    html.Div(["Uplaod Info"], className="card-title"),
+                                    html.Div(
+                                        [
+                                            html.H5(f"Filename : {filename}"),
+                                            html.H6(
+                                                f"Date : {datetime.datetime.fromtimestamp(date)}"
+                                            ),
+                                            html.Label("Upload Data"),
+                                            dash_table.DataTable(
+                                                data=df.reset_index().to_dict('records'),
+                                                columns=[
+                                                    {'name': i, 'id': i}
+                                                    for i in df.reset_index().columns
+                                                ],
+                                                style_table={'overflowX': 'scroll'},
+                                            ),
+                                        ],
+                                        className="card-content",
+                                    ),
+                                ],
+                                className="card",
+                            )
+                        ],
+                        className="col s5",
+                    ),
+                    html.Div(
+                        [
+                            html.Div(
+                                [
+                                    html.Div(["Matchup"], className="card-title"),
+                                    html.Div(
+                                        [
+                                            html.H4(f"{applicants[0]} VS {applicants[1]}"),
+                                            html.H5(f"Winner : {winner}"),
+                                            html.Label("Results"),
+                                            dash_table.DataTable(
+                                                data=results.to_dict('records'),
+                                                columns=[
+                                                    {'name': i, 'id': i} for i in results.columns
+                                                ],
+                                                style_table={'overflowX': 'scroll'},
+                                            ),
+                                        ],
+                                        className="card-content",
+                                    ),
+                                ],
+                                className="card",
+                            )
+                        ],
+                        className="col s7",
+                    ),
+                ],
+                className="row",
+            )
+        ]
+    )
+
+
+# # Callback for Stepper
+# @app.callback(Output('upload_div', 'style'), [Input('input-stepper', 'activeStep')])
+# def stepper_callback(activeStep: int):
+#     print(activeStep)
+#     if activeStep == 0:
+#         return {'display': 'block'}
+#     else:
+#         return {'display': 'none'}
+
+
+# @app.callback(Output('dropdown_1', 'options'), [Input('upload-data', 'contents')])
+# def update_output(contents):
+#     content_type, content_string = contents.split(',')
+
+#     decoded = base64.b64decode(content_string)
+#     # Assume that the user uploaded a CSV file
+#     df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+#     df.set_index('Name', inplace=True)
+#     options = list()
+#     for name in list(df.index):
+#         options.append({"label": name, "value": name})
+#     print(options)
+#     return options
+
+
+@app.callback(
+    Output('output-data-upload', 'children'),
+    [Input('upload-data', 'contents')],
+    [State('upload-data', 'filename'), State('upload-data', 'last_modified')],
+)
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d)
+            for c, n, d in zip(list_of_contents, list_of_names, list_of_dates)
+        ]
+        return children
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ brawl.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import numpy as np
+import random
+import dash_html_components as html
+
+# Function to randomly pick two applicants
+def pick_applicants(df):
+    '''
+    Input : a Pandas DataFrame
+    '''
+    assert df.index.name == "Name"
+    # Drop NAs
+    df.dropna(inplace=True)
+    # Count # of rows
+    num_rows = len(df)
+    # Pick two applicants (pseudo-random)
+    applicants = random.sample(set(list(df.index)), 2)
+    random.shuffle(applicants)
+
+    return applicants
+
+
+# A Class to Represent a Given Applicant
+class Applicant:
+    '''
+    Input : A DataFrame Row, applicant name
+    '''
+
+    # Initializer / Instance attributes
+    def __init__(self, df, applicant_name):
+        assert df.index.name == "Name"
+        assert applicant_name in list(df.index), 'Name is Not in DF!'
+        dff = df.loc[df.index == applicant_name].copy()
+        self.name = applicant_name
+        self.health = list(dff.Health)[0]
+        self.damage = list(dff.Damage)[0]
+        self.attacks = list(dff.Attacks)[0]
+        self.dodge = list(dff.Dodge)[0]
+        self.critical = list(dff.Critical)[0]
+        self.initiative = list(dff.Initiative)[0]
+
+
+def fight(round_num, applicant_pairing):
+    log = pd.DataFrame({"Round": [], "Step": [], "Action": []})
+    children = list()
+    num_round_steps = max([applicant_pairing[0].attacks, applicant_pairing[1].attacks])
+
+    random.shuffle(applicant_pairing)
+    a = applicant_pairing[0]
+    b = applicant_pairing[1]
+
+    a_attacks_original = a.attacks
+    b_attacks_original = b.attacks
+    for _ in range(num_round_steps):
+        if a.attacks > 0 and b.health > 0 and a.health > 0:
+            # print(
+            #     f'''{a.name} hits {b.name} for {a.damage} damage ({b.health}->{b.health-a.damage})  '''
+            # )
+            log = log.append(
+                {
+                    "Round": round_num,
+                    "Step": _,
+                    "Action": f'''{a.name} hits {b.name} for {a.damage} damage ({b.health}->{b.health-a.damage})  ''',
+                },
+                ignore_index=True,
+            )
+
+            children.append(html.Hr())
+            a.attacks -= 1
+            b.health -= a.damage
+
+        # B HITS A
+        # CHECK IF B HAS ATTACKS AND IF A CAN TAKE DAMAGE
+        if b.attacks > 0 and a.health > 0 and b.health > 0:
+            # print(
+            #     f'''{b.name} hits {a.name} for {b.damage} damage ({a.health}->{a.health-b.damage})  '''
+            # )
+            log = log.append(
+                {
+                    "Round": round_num,
+                    "Step": _,
+                    "Action": f'''{b.name} hits {a.name} for {b.damage} damage ({a.health}->{a.health-b.damage})  ''',
+                },
+                ignore_index=True,
+            )
+
+            b.attacks -= 1
+            a.health -= b.damage
+    a.attacks = a_attacks_original
+    b.attacks = b_attacks_original
+    return log
+
+
+def determine_winner(applicants):
+    '''
+    Input : List of Two applicants
+    Returns : Name of Winner
+    '''
+    if applicants[0].health > applicants[1].health:
+        return applicants[0].name
+    if applicants[1].health > applicants[0].health:
+        return applicants[1].name
